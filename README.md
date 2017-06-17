a4296be028fcba1c7f2bfac5b1c9ec78591a576a
taptap <alexey.e.egorov@gmail.com>
2017-06-17 20:12:52 +0300

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+Grab any X11 window with `ffmpeg`.
+
+Usage
+-----
+
+    ./windowgrab.sh
+
+Press ^C to stop grabbing. Also moving window around will break the recording.
+After you have finished, file `grab-03-13-2017-11:34:01.mp4` will apper in current directory.
---
+++ windowgrab.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+xwininfo=$(xwininfo)
+if [[ $? -ne 0 ]]; then
+    echo "Failed to get windows info" >&2
+    exit 1
+fi
+
+corner=$(echo $xwininfo | sed 's/^.*Corners://' | cut -d' ' -f2 | sed 's/\(+[0-9]*\)+/\1,/')
+w=$(echo "$xwininfo" | grep -E '^\s*Width:' | cut -d' ' -f4)
+h=$(echo "$xwininfo" | grep -E '^\s*Height:' | cut -d' ' -f4)
+w=$((w + w % 2))
+h=$((h + h % 2))
+time=$(date +"%F-%T")
+echo "Now please move mouse pointer away from window!"
+sleep 2
+ffmpeg -f x11grab -video_size ${w}x${h} -r 60 -i :0.0${corner} -qscale 0 -vcodec huffyuv /tmp/grab-${time}.avi
+ffmpeg -i /tmp/grab-${time}.avi -c:v libx264 -crf 19 -preset slow -c:a libfdk_aac -b:a 192k -ac 2 ./grab-${time}.mp4
+rm -f /tmp/grab-${time}.avi
